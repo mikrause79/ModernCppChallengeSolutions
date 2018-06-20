4f1a0827d92526142684e00b155f63d6d2be015d
Michael Krause <mikrause@ntdev.microsoft.com>
2018-06-20 15:29:37 -0700

    Make problem 1 compiler, add menu

--- ModernCppChallenge/MathProblems.cpp
+++ ModernCppChallenge/MathProblems.cpp
@@ -3,9 +3,13 @@
 // 1. Write a program that calculates and prints the sum of all the natural numbers divisible by either 3 or 5, up to a given limit entered by a user.
 int SumOfNaturalsDivisibleBy3And5(int limit)
 {
-	// A for loop and if/else is definitely clearer; using std::accumulate to exercise more modern C++ knowldege.
-	return std::accumulate(1, limit, 0, [](int currentSum, int naturalNumber)
+	int sum = 0;
+	for (int naturalNumber = 1; naturalNumber <= limit; naturalNumber++)
 	{
-		return currentSum + ((((naturalNumber % 3) == 0) || ((naturalNumber % 5) == 0)) ? naturalNumber : 0);
+		if (((naturalNumber % 3) == 0) || ((naturalNumber % 5) == 0))
+		{
+			sum += naturalNumber;
+		}
 	}
+	return sum;
 }
