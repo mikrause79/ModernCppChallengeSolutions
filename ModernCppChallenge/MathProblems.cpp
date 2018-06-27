0a965ff7ef1bfa10c2da7f7548afde0a04a9b6d1
Michael Krause <mikrause@ntdev.microsoft.com>
2018-06-27 14:53:30 -0700

    Problem 2 - GCD

--- ModernCppChallenge/MathProblems.cpp
+++ ModernCppChallenge/MathProblems.cpp
@@ -1,5 +1,6 @@
 #include <numeric>
 
+
 // 1. Write a program that calculates and prints the sum of all the natural numbers divisible by either 3 or 5, up to a given limit entered by a user.
 int SumOfNaturalsDivisibleBy3And5(int limit)
 {
@@ -12,4 +13,25 @@ int SumOfNaturalsDivisibleBy3And5(int limit)
 		}
 	}
 	return sum;
+}
+
+// 2. Write a program that, given two positive integers, will calculate and print the greatest common divisor of the two.
+int GreatestCommonDivisor(std::pair<int, int> numbers)
+{
+	int first = numbers.first;
+	int second = numbers.second;
+	if (first < second)
+	{
+		std::swap(first, second);
+	}
+
+	// Euclid's algorithm
+	int remainder = 0;
+	do
+	{
+		remainder = first % second;
+		first = second;
+		second = remainder;
+	} while (remainder != 0);
+	return first;
 }
